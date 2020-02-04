20da139444161073107a91c976d1e81d5c11cb75
vagabondtechie <vagabondtechie@gmail.com>
2020-02-05 00:22:32 +0530

    Extract and show tags for each item
    Closes #2

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 
+const TAG_PATTERN = /#[a-zA-Z0-9_]+[\s,\.\?\!]?/g;
+const TAG_TEXT_PATTERN = /#([a-zA-Z0-9_]+)[\s,\.\?\!]?/;
+
 @Component({
   selector: "app-list",
   templateUrl: "./list.component.html",
@@ -7,22 +10,40 @@ import { Component, OnInit } from "@angular/core";
 })
 export class ListComponent implements OnInit {
   items = [];
+  allTags = [];
   constructor() {}
 
   ngOnInit() {}
 
   addNewItem(element: HTMLInputElement) {
-    console.log(element);
     let value = element.value;
     if (!value || value === "") return;
-    this.items.push(this.newItem(value));
+    let newItem = this.createNewItem(value);
+    this.updateTagMasterList(newItem.tags);
+    this.items.push(newItem);
     element.value = "";
   }
 
-  newItem(text) {
+  createNewItem(text) {
     return {
       created_date: new Date(),
-      text: text
+      text: this.stripTags(text),
+      tags: this.extractTags(text)
     };
+  }
+
+  updateTagMasterList(newTags: string[]) {
+    this.allTags.push(
+      ...(newTags || []).filter(newTag => this.allTags.indexOf(newTag) === -1)
+    );
+  }
+
+  stripTags(text: string) {
+    return text.replace(TAG_PATTERN, "");
+  }
+
+  extractTags(text: string) {
+    let tags = text.match(TAG_PATTERN) || [];
+    return tags.map(tag => tag.match(TAG_TEXT_PATTERN)[1].toLowerCase());
   }
 }
