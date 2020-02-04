43f5a49b269d5a3c5efed72681310e4ed40bc107
vagabondtechie <vagabondtechie@gmail.com>
2020-02-04 18:40:40 +0530

    Added basic item addition functionality
    Closes #1

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  selector: "app-list",
+  templateUrl: "./list.component.html",
+  styleUrls: ["./list.component.css"]
 })
 export class ListComponent implements OnInit {
+  items = [];
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  addNewItem(element: HTMLInputElement) {
+    console.log(element);
+    let value = element.value;
+    if (!value || value === "") return;
+    this.items.push(this.newItem(value));
+    element.value = "";
   }
 
+  newItem(text) {
+    return {
+      created_date: new Date(),
+      text: text
+    };
+  }
 }
